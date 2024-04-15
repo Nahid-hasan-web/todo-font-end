e895f503650dd7397f15b5d289f6579f0969fabf
Nahid Hasan <nahidhasanniyan@gmail.com>
2024-04-15 10:48:25 +0600

    js done

--- js/script.js
+++ js/script.js
@@ -3,14 +3,19 @@ let todoInput      = document.querySelector('.todoInput')
 let addButton      = document.querySelector('.addButton')
 let todoList       = document.querySelector('.todoList')
 let turn           = true
-let taskDoneNumber = document.querySelectorAll('.taskDoneNumber')
-let totalTaskNumber = document.querySelectorAll('.totalTaskNumber')
+let taskDoneNumber = document.querySelector('.taskDoneNumber')
+let totalTaskNumber= document.querySelector('.totalTaskNumber')
+let doneNumber     = 0
+let totalNumber    = 0
 // ============== functions part =============
 addButton.addEventListener('click' , ()=>{
     //========== condition part start
     if(todoInput.value == ''){
         alert('please enter an value')
     }else{
+        // ======== todo number  count
+        totalNumber++
+        totalTaskNumber.innerHTML = totalNumber
         //=========== creating elements
         let singel_list   = document.createElement('div')
         let done          = document.createElement('div')
@@ -36,40 +41,38 @@ addButton.addEventListener('click' , ()=>{
         todoInput.value = ''
         // ========= edit input data
         editButton.addEventListener('click', ()=>{
-            if(turn == true){
+            if(turn == true){                
                 todoListInput .removeAttribute('readonly' , 'readonly')
                 editButton    .classList.remove('fa-regular', 'fa-pen-to-square', 'listIcon' ,'eiditIcon')
                 editButton    .classList.add('fa-regular' , 'fa-circle-check' ,'eiditIcon' , 'listIcon')
                 turn = false
             }else{
-                todoListInput .setAttribute('readonly' , 'readonly')
-                editButton    .classList.remove('fa-regular' , 'fa-circle-check' ,'eiditIcon' , 'listIcon')
-                editButton    .classList.add('fa-regular', 'fa-pen-to-square', 'listIcon' ,'eiditIcon')
-                turn = true
+                if(todoListInput.value == '' ){
+                    alert('please enter the new task')
+                }else{
+                 todoListInput .setAttribute('readonly' , 'readonly')
+                 editButton    .classList.remove('fa-regular' , 'fa-circle-check' ,'eiditIcon' , 'listIcon')
+                 editButton    .classList.add('fa-regular', 'fa-pen-to-square', 'listIcon' ,'eiditIcon')
+                 turn = true
+                }
             }
         }) 
         //========= delete todo list
         deleteButton.addEventListener('click' , ()=>{
             singel_list.remove()
+            totalNumber--
+            totalTaskNumber.innerHTML = totalNumber
+            doneNumber--
+            taskDoneNumber.innerHTML = doneNumber
         })
-        //========= dont todo task
+        //========= done todo task
         done.addEventListener('click' , ()=>{
-            done.style = 'border-color: #71ff05 ; background: #71ff05'
+            done.style = 'border-color: #71ff05 ; background: #71ff05; pointer-events: none;'
             todoListInput.style = ' text-decoration: line-through ; font-style:italic; color: #71ff05'
             editButton.style  = 'display: none'
+            doneNumber++
+            taskDoneNumber.innerHTML = doneNumber
         })
-
-     }
-
-
-
-
-
-
-
-
+    }
 })
-// ================= console part 
-
-
 
